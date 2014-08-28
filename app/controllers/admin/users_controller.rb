3865363c7dd7a5de5ce34c7ad60cea6e2d888fbc
kossgreim <ikossgreim@gmail.com>
2014-08-28 08:34:15 +0200

    fix users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  layout :admin
+
   # GET /admin/users
   def index
     @users = User.all
